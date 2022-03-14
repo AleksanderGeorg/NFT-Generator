aaa28044cb1a996a6921469fc78a9d103f90554b
AleksanderGeorg <aleksandergeorg.kikas@gmail.com>
2022-03-14 14:10:04 +0000

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ try {
   if (error.code === "MODULE_NOT_FOUND" && error.message.includes(`Cannot find module './${config.blockchain}Generator.js'`)) {
     const supported = require("./supported.json")
     console.log(`\u001b[1;31mConfig is set to generate NFTs on an invalid blockchain - \u001b[1;33m${config.blockchain}\u001b[0m\nCurrently supported (blockchain - standard):\n${supported.blockchains.join("\n")}`)
-  } else if (error.code === "MODULE_NOT_FOUND" && error.message.includes("Cannot find module 'sharp'")) {
+  } else if (error.code === "MODULE_NOT_FOUND") {
     console.log(`\u001b[1;31mYou have not done \`npm install\`\u001b[0m`)
   } else {
     console.log(error)
