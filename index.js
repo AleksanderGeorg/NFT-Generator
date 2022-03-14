a4326a8bc7a944926ab2b7f68e2ffb718d2a5f64
AleksanderGeorg <aleksandergeorg.kikas@gmail.com>
2022-03-14 12:32:24 +0000

    Error coloring

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ try {
 } catch (error) {
   if (error.code === "MODULE_NOT_FOUND" && error.message.includes(`Cannot find module './${config.blockchain}Generator.js'`)) {
     const supported = require("./supported.json")
-    console.log(`\u001b[1;31mConfig is set to generate NFTs on an invalid blockchain - ${config.blockchain}\u001b[0m\nCurrently supported (blockchain - standard):\n${supported.blockchains.join("\n")}`)
+    console.log(`\u001b[1;31mConfig is set to generate NFTs on an invalid blockchain - \u001b[1;33m${config.blockchain}\u001b[0m\nCurrently supported (blockchain - standard):\n${supported.blockchains.join("\n")}`)
   } else if (error.code === "MODULE_NOT_FOUND" && error.message.includes("Cannot find module 'sharp'")) {
     console.log(`\u001b[1;31mYou have not done \`npm install\`\u001b[0m`)
   } else {
