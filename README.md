e9b55ac14df46b06deda7aa69e045c7b0cf1f9cf
AleksanderGeorg <aleksandergeorg.kikas@gmail.com>
2022-03-28 11:50:31 +0100

    Added option to pass the config file as parameter to the generate script

--- README.md
+++ README.md
@@ -3,9 +3,9 @@ NFT Generator
 Simple NFT generator for various blockchains.
 
 How to use for generating:
-1. Set up config.json
+1. Set up config.json or any number of <FILENAME>.config.json
 2. Import assets into folder "assets" (you can delete .gitkeep if you want)
-3. Run in console `npm run generate`
+3. Run in console `npm run generate` or `npm run generate -- <FILENAME>.config.json`
 4. Matadata and NFT images will be generated into folder "out"
 
 How to use for matching metadata with NFT images:
--- generate.js
+++ generate.js
@@ -1,17 +1,20 @@
 let config
 try {
-  config = require("./config.json")
+  for (let i = 0; i < process.argv.length; i++) {
+    if (process.argv[i].includes(".config.json")) config = require(`./${process.argv[i]}`)
+  }
+  if (config === undefined) config = require("./config.json")
 } catch (error) {
-  if (error.code === "MODULE_NOT_FOUND" && error.message.includes("Cannot find module './config.json'")) {
-    console.log(`\u001b[1;31mYou have not set up config.json\u001b[0m`)
+  if (error.code === "MODULE_NOT_FOUND" && error.message.match(/Cannot find module '.\/(.*?)config.json'/).length > 0) {
+    console.log(`\u001b[1;31mYou have not set up a config.json or <FILENAME>.config.json\u001b[0m`)
   } else {
     console.log(error)
-    console.log(`\u001b[1;31mUnknown error with requiring config.json\u001b[0m`)
+    console.log(`\u001b[1;31mUnknown error with requiring a config file!\u001b[0m`)
   }
 }
 
 try {
-  if (config != undefined) {
+  if (config !== undefined) {
     const generator = require(`./${config.blockchain}Generator.js`)
     generator.run(config)
   }
@@ -23,6 +26,6 @@ try {
     console.log(`\u001b[1;31mYou have not done \`npm install\`\u001b[0m`)
   } else {
     console.log(error)
-    console.log(`\u001b[1;31mUnknown error in config.json\u001b[0m`)
+    console.log(`\u001b[1;31mUnknown error in your config file!\u001b[0m`)
   }
 }
